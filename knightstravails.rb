385e9ff46ebe181ac0187ed550aa7d53465d7e7f
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-23 12:46:00 +0200

    Improve board display and retain index

--- knightstravails.rb
+++ knightstravails.rb
@@ -5,10 +5,10 @@ class Board
     i = 0
     while i < 64
       if i + 1 > 9
-        @board << Field.new("#{i + 1}")
+        @board << Field.new("#{i}")
         i += 1
       else
-        @board << Field.new("0#{i + 1}")
+        @board << Field.new("#{i}")
         i += 1
       end
     end
@@ -18,15 +18,32 @@ class Board
     i = 0
     breaker = 1
     while i < 64
-      if breaker == 8
+      if breaker == 8 && i < 10
+        print " 0#{@board[i].number} \n\n"
+        breaker = 1
+      elsif breaker == 8
         print " #{@board[i].number} \n\n"
         breaker = 1
+      elsif i < 10
+        print " 0#{@board[i].number} "
+        breaker += 1
       else
         print " #{@board[i].number} "
         breaker += 1
       end
       i += 1
     end
+  end
+
+  def knight_moves(start, goal)
+    start_val = start[0] * 8 + start[1]
+    goal_val = goal[0] * 8 + goal[1]
+
+    start_field = @board[start_val]
+    goal_field = @board[goal_val]
+
+    puts start_field.number
+    puts goal_field.number
   end
 end
 
@@ -40,3 +57,5 @@ end
 
 board = Board.new
 board.print_board
+
+board.knight_moves([0, 0], [1, 1])
