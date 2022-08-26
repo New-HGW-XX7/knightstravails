fccc67981e424dbc2a82f37b31c28e0497b07878
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-26 19:44:57 +0200

    Outputs path
    Had the right idea from the get-go today.
    Tree builiding recursively.
    Path retrieval with BFS.
    Linking nodes to their parents.
    
    I only lacked proper implementation and had to be reaffirmed
    by one of the solutions (a readme, not actual code).

--- knightstravails_5.rb
+++ knightstravails_5.rb
@@ -35,13 +35,13 @@ class Node
     @c_6 = nil
     @c_7 = nil
     @c_8 = nil
-    @children = [@c_1, @c_2, @c_3, @c_4, @c_5, @c_6, @c_7, @c_8]
+    #@children = [@c_1, @c_2, @c_3, @c_4, @c_5, @c_6, @c_7, @c_8]
   end
 end
 
-def build_tree(start, final, i = 0)
+def build_tree(start, final, i = 0, parent = nil)
   node = Node.new(start)
-  
+  node.parent = parent
   return node if i == 6
 
   moves = find_legal_coordinates(node.value)
@@ -52,31 +52,66 @@ def build_tree(start, final, i = 0)
   
   return node if node.value == final
 
-  node.c_1 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_1 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
   
-  node.c_2 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_2 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_3 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_3 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_4 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_4 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_5 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_5 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_6 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_6 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_7 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_7 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
 
-  node.c_8 = build_tree(moves.shift, final, i + 1) unless moves.empty?
+  node.c_8 = build_tree(moves.shift, final, i + 1, node) unless moves.empty?
   
   node
 end
 
+def bfs(tree, final)
+  q = [tree]
+  target = nil
 
+  found = false
+
+  until found == true
+    if q.first.value == final
+      target = q.first
+      found = true
+    else
+      q << q.first.c_1 unless q.first.c_1.nil?
+      q << q.first.c_2 unless q.first.c_2.nil?
+      q << q.first.c_3 unless q.first.c_3.nil?
+      q << q.first.c_4 unless q.first.c_4.nil?
+      q << q.first.c_5 unless q.first.c_5.nil?
+      q << q.first.c_6 unless q.first.c_6.nil?
+      q << q.first.c_7 unless q.first.c_7.nil?
+      q << q.first.c_8 unless q.first.c_8.nil?
+    end
+    q.shift
+  end
+
+  path = []
+  
+  until target.value == tree.value
+    path.unshift(target.value)
+    target = target.parent
+  end
+
+  path.unshift(tree.value)
+  
+  puts "You made it in #{path.length - 1} steps."
+  puts "Your path is #{path}"
+end
 
 
 
 tree = build_tree([3, 3], [7, 7])
 
+bfs(tree, [7, 7])
 
 
 
